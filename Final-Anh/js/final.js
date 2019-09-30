0b2feb518b1ff817c8c274e4c95486f67068a047
Hồ Trung Anh <hotrunganhht1912@gmail.com>
2019-09-30 17:16:22 +0700

    fix nhe

--- Final-Anh/js/final.js
+++ Final-Anh/js/final.js
@@ -1,19 +1,22 @@
 function menuOpen() {
-	var x = document.getElementById("menu-top-element") //lấy các giá trị theo  id
-	if(x.className === "menu-top"){ // neu class rong them  call responsive
-		x.className += " responsive";
-	}else{
-		x.className = "menu-top";
-	}
+    var x = document.getElementById("menu-top-element") //lấy các giá trị theo  id
+    if (x.className === "menu-top") { // neu class rong them  call responsive
+        x.className += " responsive";
+    } else {
+        x.className = "menu-top";
+    }
 }
+
 function menuOpen2() {
-	var y = document.getElementById("search-form") //lấy các giá trị theo  id
-	if(y.className === "search-form"){ // neu class rong them  call responsive
-		y.className += " responsive";
-	}else{
-		y.className = "search-form";
-	}
+    var y = document.getElementById("search-form") //lấy các giá trị theo  id
+    if (y.className === "search-form") { // neu class rong them  call responsive
+        y.className += " responsive";
+    } else {
+        y.className = "search-form";
+    }
 }
+
 $('.carousel').carousel({
-  interval: 1000
-})
+    interval: 10000,
+    pause: "false"
+});
