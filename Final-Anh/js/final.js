116db133b07dd94a36f8e76c644071ee3ed126ed
Hồ Trung Anh <hotrunganhht1912@gmail.com>
2019-09-30 15:09:33 +0700

    fix-img

---
+++ Final-Anh/js/final.js
@@ -0,0 +1,16 @@
+function menuOpen() {
+	var x = document.getElementById("menu-top-element") //lấy các giá trị theo  id
+	if(x.className === "menu-top"){ // neu class rong them  call responsive
+		x.className += " responsive";
+	}else{
+		x.className = "menu-top";
+	}
+}
+function menuOpen2() {
+	var y = document.getElementById("search-form") //lấy các giá trị theo  id
+	if(y.className === "search-form"){ // neu class rong them  call responsive
+		y.className += " responsive";
+	}else{
+		y.className = "search-form";
+	}
+}
